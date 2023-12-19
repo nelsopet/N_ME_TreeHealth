893540d8f6e74271214682a87d9ca670df45fdb8
nelsopet <peter.ross.nelson@gmail.com>
2023-12-19 12:31:31 -0500

    projected in UTM zone 19

--- scripts/UMFK_TreeLoc_DMS_convert_DD_export_to_shp.R
+++ scripts/UMFK_TreeLoc_DMS_convert_DD_export_to_shp.R
@@ -21,7 +21,8 @@ colnames(Picea_2022)
 colnames(Populus_2021)
 
 #Get CRS from Headwall image for reprojecting shapefile coordinates
-img<-terra::rast("/Users/peternelson 1/Documents/Schoodic/lecospec_at_schoodic/Git/lecospec/Data/Ground_Validation/Imagery/BisonGulchQuads.envi")
+#Getting CRS from AK image not working ! Duh. Use Maine image.
+#img<-terra::rast("/Users/peternelson 1/Documents/Schoodic/lecospec_at_schoodic/Git/lecospec/Data/Ground_Validation/Imagery/BisonGulchQuads.envi")
 terra::crs(img)
 
 #Filter out trees without location information
@@ -89,18 +90,22 @@ Picea_2022<-filter(Picea_2022, is.na(lon_dd)==FALSE) %>%
 #Add NAD83 datum to lat_dd and lon_dd
 #Populus_2021_sf<-sf::st_as_sf(Populus_2021,coords = c("lon_dd","lat_dd"), dim="XY",crs = 4269)
 Populus_2021_vect<-terra::vect(Populus_2021,geom = c("lon_dd","lat_dd"))
-terra::crs(Populus_2021_vect)<-"EPSG:4269"
+terra::crs(Populus_2021_vect)<-"EPSG:4326"
 Populus_2021_vect_WGS_84<-terra::project(Populus_2021_vect, "EPSG:4328")
-Populus_2021_vect_WGS_84_vec_proj<-terra::project(Populus_2021_vect_WGS_84, img)
-terra::crs(Populus_2021_vect_WGS_84_vec_proj)
-
+#Populus_2021_vect_WGS_84_vec_proj<-terra::project(Populus_2021_vect_WGS_84, img)
+terra::crs(Populus_2021_vect_WGS_84)#_vec_proj)
+proj_crs<-"EPSG:32619" #WGS84 projected inZone 19N in Maine
+Populus_2021_vect_WGS_84_UTM<-terra::project(Populus_2021_vect_WGS_84, proj_crs)
+terra::crs(Populus_2021_vect_WGS_84_UTM)
 #Populus_2021_sf_WGS84<-sf::st_transform(Populus_2021_sf, crs = 4328)
 #Populus_2021_sf_WGS84_vec<-vect(Populus_2021_sf_WGS84)
 
 Picea_2022_sf<-sf::st_as_sf(Picea_2022,coords = c("lon_dd","lat_dd"), dim="XY",crs = 4269)
-Picea_2022_sf_WGS84<-sf::st_transform(Picea_2022_sf, crs = 4328)
+Picea_2022_sf_WGS84<-sf::st_transform(Picea_2022_sf, crs = 4326)
 Picea_2022_sf_WGS84_vec<-vect(Picea_2022_sf_WGS84)
-Picea_2022_sf_WGS84_vec_proj<-terra::project(Picea_2022_sf_WGS84_vec, img)
+Picea_2022_sf_WGS84_vec_proj<-terra::project(Picea_2022_sf_WGS84_vec, proj_crs)
+
+terra::crs(Picea_2022_sf_WGS84_vec_proj)
 
 #crs_use<-terra::geom(vect(Populus_2021_sf_WGS84))
 #tree_locs_2023_vec<-terra::vect(tree_locs_2023, geom = c("lon_dd", "lat_dd"), crs = crs_use, keepgeom  = TRUE)
@@ -136,9 +141,8 @@ leaflet(tree_locs_2022_sf_WGS84_spat) %>%
 
 #sf::st_write(tree_locs_2023_sf, "output/UMFK_MEIFSCI_tree_locations_2023.shp", dsn = "geometry", driver = "ESRI Shapefile", append = TRUE)
 #sf::st_write(tree_locs_2023_sf_WGS84, "output/UMFK_MEIFSCI_tree_2023_WGS84.shp", driver = "ESRI Shapefile", append = TRUE)
-terra::writeVector(Populus_2021_vect_WGS_84_vec_proj, filename = "output/Populus_2021_sf_WGS84.shp", filetype= "ESRI Shapefile", overwrite = TRUE)
+terra::writeVector(Populus_2021_vect_WGS_84_UTM, filename = "output/Populus_2021_sf_WGS84.shp", filetype= "ESRI Shapefile", overwrite = TRUE)
 #terra::writeVector(Populus_2021_vect_WGS_84, filename = "output/Populus_2021_sf_WGS84.json", filetype= "GeoJSON", overwrite = TRUE)
 
 terra::writeVector(Picea_2022_sf_WGS84_vec_proj, filename = "output/Picea_2022_sf_WGS84.shp", filetype= "ESRI Shapefile", overwrite = TRUE)
 
-terra::crs(Picea_2022_sf_WGS84_vec_proj)
