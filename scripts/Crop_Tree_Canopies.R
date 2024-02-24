374e4a5f61e6a0c04bc379a05354d403f44d4f01
nelsopet <peter.ross.nelson@gmail.com>
2024-02-24 12:42:47 -0500

    crop tree canopies by site out of each image and write canopy spectra to output

--- scripts/Crop_Tree_Canopies.R
+++ scripts/Crop_Tree_Canopies.R
@@ -15,29 +15,39 @@ CL2_1426_image_path= "G:/CL22022/100539_cl2_2022_2022_07_20_16_30_54/raw_1426_rd
 CL2_3615_image_path = "G:/CL22022/100539_cl2_2022_2022_07_20_16_30_54/raw_3426_rd_rf_or"
 HB1_9252_image_path = "E:/Hewes_Brook_1/Imaging_Spectrometer/100464_hb1_2022_flight1_2022_06_28_15_53_13/raw_9253_rd_rf_or"
 HB1_9918_image_path = "E:/Hewes_Brook_1/Imaging_Spectrometer/100464_hb1_2022_flight1_2022_06_28_15_53_13/raw_9918_rd_rf_or"
+HB3_5624_image_path = "E:/Hewes_Brook_1/Imaging_Spectrometer/100471_hewes_brook_3_2022_flight2_2022_06_28_18_20_28/raw_5624_rd_rf_or"
+HB3_7624_image_path = "E:/Hewes_Brook_1/Imaging_Spectrometer/100471_hewes_brook_3_2022_flight2_2022_06_28_18_20_28/raw_7624_rd_rf_or"
+RR1_2250_image_path = "E:/Red_River_1/Imaging_Spectrometer/100479_rr1_2022_flight2_2022_06_28_19_58_04/raw_2250_rd_rf_or"
+RR1_4267_image_path = "E:/Red_River_1/Imaging_Spectrometer/100479_rr1_2022_flight2_2022_06_28_19_58_04/raw_4267_rd_rf_or"
+
+spruce_imgs<-c(
+CL2_1426_image_path
+,CL2_3615_image_path
+,HB1_9252_image_path
+,HB1_9918_image_path
+,HB3_5624_image_path
+,HB3_7624_image_path
+,RR1_2250_image_path
+,RR1_4267_image_path
+,SR1_0_image_path
+,SR1_1428_image_path)
 
 canopies_path = "raw_data/geometry/hand_drawn/"
-list.files(canopies_path)
-#bring in test image
-img <- brick(image_path)
-
-#bring in vector of canopies
-#canopies_vec <- readOGR(dsn = paste0(canopies_path, "100038_PEF_SM_4_110m_2019_06_18_15_16_13_ortho_raw_7492_rd_rf_or.shp"))
-canopies_full <- readOGR(dsn = paste0(canopies_path, "full_Howland_100006_2000.shp"))
-canopies_light <- readOGR(dsn = paste0(canopies_path, "light_Howland_100006_2000.shp"))
-canopies_shadow <- readOGR(dsn = paste0(canopies_path, "shadow_Howland_100006_2000.shp"))
-
+canopies_sites<-list.files(canopies_path)[grepl("*.shp$",list.files(canopies_path))]
+#canopies<-terra::vect(paste(canopies_sites[1],canopies_sites[1])
+canopies<-lapply(1:length(canopies_sites), function(x) {terra::vect(paste(canopies_path, canopies_sites[x], sep=""))})
+#canopies<-Reduce(c,canopies)
 #Now, crop and mask image by each canopy shapefile. Functions are split into
   #full canopies, illuminated only, and shadowed only
 #FULL CANOPIES#
-lapply(1:length(canopies_full),  
+lapply(1:length(spruce_imgs),  
        function(x) {
-         tst_img <- brick(image_path)
+         tst_img <- terra::rast(spruce_imgs[x])
          tst_names<-names(tst_img)
-         tst_quads<-canopies_full[x,]
-         tst_crop <- raster::crop(tst_img, tst_quads)
-         tst_mask <- raster::mask(tst_crop, tst_quads)
-         metadata(tst_mask)<-as.list(canopies_full[x,]$CLASS_NAME)
+         tst_quads<-canopies[[x]]
+         tst_crop <- terra::crop(tst_img, tst_quads)
+         tst_mask <- terra::mask(tst_crop, tst_quads)
+         #metadata(tst_mask)<-tst_quads$CLASS_NAME
          bandnames(tst_mask)<-tst_names
-         writeRaster(tst_mask, paste("M:\\MSGC_DATA\\Howland\\Cropped_ROIs_spectra\\", canopies_full[x,]$CLASS_NAME, sep=""), format = "raster", overwrite = TRUE)
+            lapply(1:length(tst_quads), function (x) {writeRaster(tst_mask, paste("output/canopy_spectra/", tst_quads[x]$CLASS_NAME, ".ENVI",sep=""), overwrite = TRUE)})
        })
